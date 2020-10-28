395fd1a26268967fb8aadd24c90aaeb0c3ab4a1d
Sin Wildy <sw.wildy@gmail.com>
2020-10-28 21:00:57 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(my_favorite_things,more_favs)
+  my_favorite_things.concat(more_favs)
+end
+
+def using_insert(list_of_programming_languages,another_language)
+  list_of_programming_languages.insert(4,another_language)
+end
+
+def using_uniq(haircuts)
+  haircuts.uniq 
+end
+
+def using_flatten(instruments)
+  instruments.flatten
+end
+
+def using_delete(instructors,no_offense_steven)
+  instructors.delete(no_offense_steven)
+end
+
+def using_delete_at(famous_robots,deleted_robot)
+  famous_robots.delete_at(deleted_robot)
+end
